94dfde859aca2cda3783039a707b3b6c45768eb5
Zbynda <jan@piroutek.info>
2019-04-01 20:54:19 +0200

    list of todo, and rewritten func for returning trips to return also round

--- funcs.py
+++ funcs.py
@@ -4,14 +4,18 @@ def get_stop_id_by_name(name, stops): # returns <stop_id> from name of the stop
 			return stop["stop_id"]
 	return None
 
-def get_trip_ids_by_stop_id(stop_id, stop_times): # returns list of <trip_id>s which stops at the stop with given stop_id
+
+def get_trip_ids_by_stop_id(stop_id, stop_times, trips_data): # returns list of <trip_id>s which stops at the stop with given stop_id
 	trip_ids = []
+	lines_id = []
 	for trip in trips:
 		if (stop_id == trip["stop_id"]):
 			trip_ids.append(trip["trip_id"])
+			lines_id.append(get_route_id_by_trip_id(trip["trip_id"], trips_data))
 	if (trip_ids == []):
 		return None
-	return trip_ids
+	return trip_ids, lines_id
+
 
 def get_route_id_by_trip_id(trip_id, trips): # returns <route_id> of given trip_id
 	for trip in trips:
@@ -19,7 +23,8 @@ def get_route_id_by_trip_id(trip_id, trips): # returns <route_id> of given trip_
 			return trip['route_id']
 	return None
 
-def get_stop_ids_with_times_by_trip_id(trip_id, stop_times): # returns dics, where value is list with times and keys are <stop_id>s
+
+def get_stop_ids_with_times_by_trip_id(trip_id, stop_times): # returns dict, where value is list with times and keys are <stop_id>s
 	stop_ids_with_times = {}
 	for i in range(len(stop_times)):
 		if stop_times[i+1]['trip_id'] == trip_id:
@@ -30,20 +35,22 @@ def get_stop_ids_with_times_by_trip_id(trip_id, stop_times): # returns dics, whe
 		return None
 	return stop_ids_with_times
 
+
 def get_time(trip_id, stop_id, stop_times): # returns (arrival_time, departure_time) on given <stop_id> and <trip_id>
 	for time in stop_times:
 		if (time['stop_id'] == stop_id and time['trip_id'] == trip_id):
 			return (time['arrival_time'], time['departure_time'])
 	return None
 
+
 def get_times(trip_ids, stop_id, stop_times): # retrun list of tuples of times on given <stop_id> and list of trip_ids
 	return [get_time(trip, stop_id, stop_times) for trip in trip_ids]
+
 
 def get_next_stop_with_times(trips_ids, stop_id, stop_times):
 	return (stop_id, get_times(trip_ids, stop_id, stop_times))
 
-def get_list_of_next_stops_with_times(trips_ids, next_stop_ids, stop_times):
-	return [get_next_stop_with_times(trips_ids, stop_id, stop_times) for stop_id in stop_ids]
-
-def get_dict_of_lines_stopsAndTimes(lines, ):
-	return {}
+# todo:
+# f(zastavka) -> [linky] and [trips]
+# g(zastavka, trips) -> [next_stops]
+# h(linka, zastavka, trips) -> [casy]
